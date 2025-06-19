727c3425c57b512af073a793d526c47da5fa851e
Rakshit <rakshit.gupta229@gmail.com>
2025-06-19 20:47:20 +0530

    Initial commit - Video Call App with frontend and backend

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import userRoutes from "./routes/users.routes.js"
+
+import { connectToSocket } from './controllers/socketManager.js';
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({extended: true, limit: "40kb"}));
+
+app.use("/api/v1/users", userRoutes);
+
+app.get("/home",(req,res) => {
+    return res.json({"hello": "World"})
+});
+
+const start = async () => {
+    const connectionDB = await mongoose.connect("mongodb+srv://krishaggarwal212:5rSx1D4FYsXwQ52H@videocallcluster.slaxcxd.mongodb.net/?retryWrites=true&w=majority&appName=VideoCallCluster");
+    console.log(`MONGO Connected DB Host: ${connectionDB.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+};
+
+start();
